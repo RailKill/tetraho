d1f5383e3a1f3add34d9da2dff9fc5f6c44f3bd6
Lemoney <railkill@gmail.com>
2020-08-16 06:42:04 +0800

    Set up initial test scene

---
+++ assets/scripts/actor.gd
@@ -0,0 +1,17 @@
+class_name Actor
+extends KinematicBody2D
+# An actor that can perform actions in the game.
+
+
+# Hit points.
+var hp = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
