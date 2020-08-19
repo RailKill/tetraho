b9f1456e0c3523e6fd3485944fc8623830eedbc8
Lemoney <railkill@gmail.com>
2020-08-20 06:46:08 +0800

    Implement basic in-game menu

---
+++ assets/scripts/ingame_menu.gd
@@ -0,0 +1,38 @@
+class_name IngameMenu
+extends Control
+
+
+# Title heading node of this in-game menu panel.
+onready var title = $Label
+# Resume button, a.k.a the first button to grab focus when menu is visible.
+onready var button_resume = $VBoxContainer/ButtonResume
+
+
+func _input(_event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if visible:
+			_on_ButtonResume_pressed()
+		else:
+			show()
+
+
+func show():
+	.show()
+	button_resume.grab_focus()
+
+
+func _on_ButtonResume_pressed():
+	hide()
+
+
+func _on_ButtonQuit_pressed():
+	get_tree().quit()
+
+
+func _on_ButtonRestart_pressed():
+	_on_ButtonResume_pressed()
+	get_tree().reload_current_scene()
+
+
+func _on_IngameMenu_visibility_changed():
+	get_tree().paused = visible
